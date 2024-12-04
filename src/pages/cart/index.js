f11d65adff2c9fc3200e5b273c1baf3a5ae9499b
renatohvo <renatohvo@gmail.com>
2024-12-03 21:26:28 -0300

    whatsapp button redirect

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -8,7 +8,17 @@ import { formatNumber } from '../../helpers/utils';
 
 const Cart = () => {
 
-    const { total, cartItems, itemCount, clearCart, checkout, handleCheckout } = useCart();
+    const { total, cartItems, itemCount, clearCart, checkout } = useCart();
+
+    async function handleWhatsApp() {
+        let pathWhatsApp = `http://wa.me/${process.env.REACT_APP_WHATSAPP}?text=Hello!%0AI%20want%20to%20order%20the%20following%20items%20from%20your%20store.%0A%0A`;
+        for (let key in cartItems) {
+            if (cartItems[key] !== null) pathWhatsApp += "Cod.:%20" + cartItems[key].id +"%20-%20" + cartItems[key].attributes.title + "%20(" + cartItems[key].quantity + "),%20%0A";
+        }
+        pathWhatsApp += "%0ATOTAL:%20R$%20" + total + ".%20%0A";
+        pathWhatsApp += "%0A%0AThank%20you!";
+        window.location.href = pathWhatsApp;
+    }
 
     return (
         <Layout title="Cart" description="This is the Cart page" >
@@ -45,7 +55,7 @@ const Cart = () => {
                                 <h3 className="m-0 txt-right">{formatNumber(total)}</h3>
                                 <hr className="my-4" />
                                 <div className="text-center">
-                                    <button type="button" className="btn btn-primary mb-2" onClick={handleCheckout}>CHECKOUT</button>
+                                    <button type="button" className="btn btn-success mb-2" onClick={handleWhatsApp}>SEND TO WHATSAPP</button>
                                     <button type="button" className="btn btn-outlineprimary btn-sm" onClick={clearCart}>CLEAR</button>
                                 </div>
 
