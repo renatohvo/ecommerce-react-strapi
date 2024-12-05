04ecef6090dd65745d879afb1ab83722d3c6e047
renatohvo <renatohvo@gmail.com>
2024-12-05 20:09:26 -0300

    search products

--- src/pages/store/ProductsGrid.js
+++ src/pages/store/ProductsGrid.js
@@ -1,30 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductItem from './ProductItem';
 import styles from './ProductsGrid.module.scss';
 import { useProducts } from '../../hooks/useProducts';
+import ProductSearch from './ProductSearch';
 
 const ProductsGrid = () => {
 
     const { products } = useProducts()
+    const [filteredProducts, setFilteredProducts] = useState(products)
+
+    const onProductSearch = (search) => {
+        if (search.trim() === '') {
+            setFilteredProducts(products)
+        } else {
+            const filtered = products.filter(product =>
+                product.attributes.title.toLowerCase().includes(search.toLowerCase())
+            )
+            setFilteredProducts(filtered)
+        }
+    };
 
     return (
         <div className={styles.p__container}>
             <div className="row">
                 <div className="col-sm-8">
                     <div className="py-3">
-                        {products.length} Products
+                        {filteredProducts.length} Products {filteredProducts.length < products.length ? `Filtered` : `Total`}
                     </div>
                 </div>
-                <div className="col-sm-4">
-                    <div className="form-group">
-                        <input type="text" name="" placeholder="Search product" className="form-control" id="" />
-                    </div>
-                </div>
+                <ProductSearch onProductSearch={onProductSearch} />
             </div>
             <div className={styles.p__grid}>
 
                 {
-                    products.map(product => (
+                    filteredProducts.map(product => (
                         <ProductItem key={product.id} product={product} />
                     ))
                 }
